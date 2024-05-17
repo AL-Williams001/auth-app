547ab56fc887dd2f5738c24f9ee0c6e153471eb6
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-05-17 15:02:16 +0800

    complete sign up page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,36 +1,82 @@
+import { set } from "mongoose";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="p-5 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
           className="bg-gray-100 border border-gray-300 rounded-lg p-3  "
+          onChange={handleChange}
+          autoComplete="current-username"
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
           className="bg-gray-100 border border-gray-300 rounded-lg p-3  "
+          onChange={handleChange}
+          autoComplete="current-email"
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
           className="bg-gray-100 border border-gray-300 rounded-lg p-3  "
+          onChange={handleChange}
+          autoComplete="current-password"
         />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg uppercase">
-          Sign Up
+        <button
+          disabled={loading}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 rounded-lg uppercase"
+        >
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
-      <p className="text-center mt-5 flex gap-2">
-        Already have an account?{" "}
-        <a href="/login" className="text-blue-500 font-semibold">
-          Login
-        </a>
-      </p>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/sign-in">
+          <span className="text-blue-500 font-semibold">Sign in</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
     </div>
   );
 };
